d684ae91a869f65dc0c6a0f9d1aaac7f9c77578a
Yuzawa <ham-and-egg@hotmail.co.jp>
2024-01-24 22:47:47 +0900

    users_table,curriculum_progress_tableのマイグレーションファイル追加

---
+++ database/migrations/2024_01_24_131226_create_curriculum_progress_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculum_progress', function (Blueprint $table) {
+            $table->id();
+            $table->integer('curriculumus_id');
+            $table->integer('users_id');
+            $table->tinyInteger('clear_flg');//クリアフラグ
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculum_progress');
+    }
+};
---
+++ database/migrations/2024_01_24_133237_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');//インクリメント
+            $table->string('name');//名前
+            $table->string('name_kana');//カナ
+            $table->string('email')->unique();//メールアドレス
+            $table->string('password');//パスワード
+            $table->string('profile_image');//プロフィール画像
+            $table->integer('classes_id');//クラスID
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
